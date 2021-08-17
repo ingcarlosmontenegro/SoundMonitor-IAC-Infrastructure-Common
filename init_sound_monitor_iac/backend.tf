c6da6658eca32b221e1aff296c15b0beb2e6e667
ingcarlosmontenegro <ingcarlosmontenegro@gmail.com>
2021-08-17 18:38:44 -0500

    Update backend.tf
    Se cambio al Bucket de la cuenta nueva.

--- init_sound_monitor_iac/backend.tf
+++ init_sound_monitor_iac/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "terraform-monitor-provide-states-files"
+    bucket  = "terraform-backend-montenegro"
     key     = "common.tfstate"
     region  = "us-east-1"
   }
