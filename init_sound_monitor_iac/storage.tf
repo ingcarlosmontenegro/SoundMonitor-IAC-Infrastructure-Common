3e2c5b5f14d00fd4d1bdd015143a1a430f806de2
ingcarlosmontenegro <ingcarlosmontenegro@gmail.com>
2021-08-17 18:21:32 -0500

    Update storage.tf
    Se cambio el nombre del Bucket para que sea único en la region.

--- init_sound_monitor_iac/storage.tf
+++ init_sound_monitor_iac/storage.tf
@@ -1,10 +1,10 @@
 resource "aws_s3_bucket" "soundmonitor-audiodata" {
-    bucket = "soundmonitor-audiodata"
+    bucket = "soundmonitor-audiodata-montenegro"
     acl    = "private"
 }
 
 resource "aws_s3_bucket" "soundmonitor-visualdata" {
-    bucket = "soundmonitor-visualdata"
+    bucket = "soundmonitor-visualdata-montenegro"
     acl    = "private"
 }
 
